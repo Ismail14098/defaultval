b2e1e25f61732ee87c928bea8af461feb799c818
Ismail Ibragim <ismail.ibragim@kmf.kz>
2024-06-05 10:39:47 +0500

    feat: string

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ismail14098/null
+
+go 1.22.3
---
+++ string.go
@@ -0,0 +1,29 @@
+package null
+
+type String struct {
+	*string
+}
+
+func StringFrom(val string) String {
+	return String{&val}
+}
+
+func StringFromPtr(val *string) String {
+	return String{val}
+}
+
+func (s String) ValueOrZero() string {
+	if s.string == nil {
+		return ""
+	}
+
+	return *s.string
+}
+
+func (s String) ValueOrDefault(def string) string {
+	if s.string == nil {
+		return def
+	}
+
+	return *s.string
+}
