9e1f6f12f88bf07f617e51589ff10e61b1b0670e
Ismail Ibragim <ismail.ibragim@kmf.kz>
2024-06-05 10:53:08 +0500

    feat: int

---
+++ int.go
@@ -0,0 +1,27 @@
+package null
+
+type Int struct{ *int }
+
+func IntFrom(val int) Int {
+	return Int{&val}
+}
+
+func IntFromPtr(val *int) Int {
+	return Int{val}
+}
+
+func (i Int) ValueOrZero() int {
+	if i.int == nil {
+		return 0
+	}
+
+	return *i.int
+}
+
+func (i Int) ValueOrDefault(def int) int {
+	if i.int == nil {
+		return def
+	}
+
+	return *i.int
+}
